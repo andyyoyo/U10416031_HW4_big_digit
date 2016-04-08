f2fc6307874a3cb3f9913133e4ba1f5eb2552be3
andyyoyo <andyyoyo76543@gmail.com>
2016-04-09 01:55:52 +0800

    Update BigDigit.java
    set the method that can apply the add method

--- BigDigit.java
+++ BigDigit.java
@@ -7,8 +7,38 @@ class BigDigit{
     public static void main(String args[]){
         
     	Scanner input = new Scanner(System.in);
+    	
+    	//prompt the user to enter the numbers
     	System.out.println("Enter the two big-digit number(The two numbers which must have the same digit):");
     	String s1 = input.nextLine();
         String s2 = input.nextLine();
         add(s1, s2);
     }
+    //set the add method
+    public static void add(String a, String b){
+        ArrayList<String> re = new ArrayList<String>();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while(true){
+            int i1 = Integer.parseInt(Character.toString(a.charAt(i)));
+            int i2 = Integer.parseInt(Character.toString(b.charAt(j)));
+            Integer i3  = i1 + i2 + c;
+            //the top digit condition
+            if(i3 > 9){
+                c = 1;
+                i3 = i3 - 10;
+            }else c = 0;
+            re.add(i3.toString());
+            i--;
+            j--;
+            if(i < 0){
+                re.add(c.toString());
+                break;
+            }
+        }
+        Collections.reverse(re);
+        for(String r : re){
+            System.out.print(r);
+        }
+    }
+} 
